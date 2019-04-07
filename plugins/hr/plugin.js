903d23cc015a1cf3f3c6f71a9a8de25c2fc2fdc7
Mohammad Rafigh <mohammad.rafigh@gmail.com>
2019-04-07 21:15:11 +0430

    Added code tag into code sample

--- plugins/hr/plugin.js
+++ plugins/hr/plugin.js
@@ -1,39 +1,39 @@
 (function () {
 var hr = (function () {
-    'use strict';
+  'use strict';
 
-    var global = tinymce.util.Tools.resolve('tinymce.PluginManager');
+  var global = tinymce.util.Tools.resolve('tinymce.PluginManager');
 
-    var register = function (editor) {
-      editor.addCommand('InsertHorizontalRule', function () {
-        editor.execCommand('mceInsertContent', false, '<hr />');
-      });
-    };
-    var Commands = { register: register };
-
-    var register$1 = function (editor) {
-      editor.addButton('hr', {
-        icon: 'hr',
-        tooltip: 'Horizontal line',
-        cmd: 'InsertHorizontalRule'
-      });
-      editor.addMenuItem('hr', {
-        icon: 'hr',
-        text: 'Horizontal line',
-        cmd: 'InsertHorizontalRule',
-        context: 'insert'
-      });
-    };
-    var Buttons = { register: register$1 };
-
-    global.add('hr', function (editor) {
-      Commands.register(editor);
-      Buttons.register(editor);
+  var register = function (editor) {
+    editor.addCommand('InsertHorizontalRule', function () {
+      editor.execCommand('mceInsertContent', false, '<hr />');
     });
-    function Plugin () {
-    }
+  };
+  var $_dnegzf92jnsufoxr = { register: register };
 
-    return Plugin;
+  var register$1 = function (editor) {
+    editor.addButton('hr', {
+      icon: 'hr',
+      tooltip: 'Horizontal line',
+      cmd: 'InsertHorizontalRule'
+    });
+    editor.addMenuItem('hr', {
+      icon: 'hr',
+      text: 'Horizontal line',
+      cmd: 'InsertHorizontalRule',
+      context: 'insert'
+    });
+  };
+  var $_fvulgu93jnsufoxs = { register: register$1 };
+
+  global.add('hr', function (editor) {
+    $_dnegzf92jnsufoxr.register(editor);
+    $_fvulgu93jnsufoxs.register(editor);
+  });
+  function Plugin () {
+  }
+
+  return Plugin;
 
 }());
 })();
